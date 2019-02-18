cc9f3fbc1cf3ce230b6c541ae413770f7e1de5b5
Viresh Gupta <viresh16118@iiitd.ac.in>
2019-02-18 16:16:45 +0530

    Update Scalar Feature classifier script
    Correct data location and add more evaluation parameters for
    buzzfeed corpus

--- old_stuff/buzz/classification_accuracy.py
+++ old_stuff/buzz/classification_accuracy.py
@@ -1,6 +1,6 @@
 import pandas as pd
 features = pd.read_csv('prediction.txt')
-truth = pd.read_csv('../../BuzzFeedData/overview.csv')
+truth = pd.read_csv('../../../BuzzFeedData/overview.csv')
 truth['XML'] = truth['XML'].apply(lambda x: int(x[:-4]))
 truth = truth[['XML', 'orientation']]
 truth['orientation'] = truth['orientation'].apply(lambda x: 0 if x == 'mainstream' else 1)
@@ -15,19 +15,46 @@ These models use features such as sentiment of article etc etc
 """
 
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
-from sklearn.metrics import accuracy_score
+from sklearn import metrics 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 import numpy as np
+import json
 
 train_X, test_X, train_y, test_y = train_test_split(X, Y, test_size=0.20)
 
+# def get_accuracy(model, trainX=train_X, testX=test_X, trainY=train_y, testY=test_y):
+# 	model.fit(trainX, trainY)
+# 	return model.score(testX, testY)
 def get_accuracy(model, trainX=train_X, testX=test_X, trainY=train_y, testY=test_y):
-	model.fit(trainX, trainY)
-	return model.score(testX, testY)
+    model.fit(trainX, trainY)
+    predictions = model.predict(testX)
+    accuracy = metrics.accuracy_score(predictions, testY)
+    precision_micro = metrics.precision_score(testY, predictions, average='micro')
+    precision_binary = metrics.precision_score(testY, predictions)
+    precision_macro = metrics.precision_score(testY, predictions, average='macro') 
+    recall_micro =  metrics.recall_score(testY, predictions, average='micro')
+    recall_binary =  metrics.recall_score(testY, predictions)
+    recall_macro =  metrics.recall_score(testY, predictions, average='macro')
+    f1_micro = metrics.f1_score(testY, predictions, average='micro')
+    f1_binary = metrics.f1_score(testY, predictions)
+    f1_macro = metrics.f1_score(testY, predictions, average='macro')
+    d = {
+        'accuracy': accuracy,
+        'precision_micro': precision_micro,
+        'precision_binary': precision_binary,
+        'precision_macro': precision_macro,
+        'recall_micro': recall_micro,
+        'recall_binary': recall_binary,
+        'recall_macro': recall_macro,
+        'f1_micro': f1_micro,
+        'f1_binary': f1_binary,
+        'f1_macro': f1_macro
+    }
+    return json.dumps(d, indent=4)
 
 def cross_validate_kfold(model, X=X, Y=Y, k=3):
 	return cross_val_score(model, X, Y, cv=k)
